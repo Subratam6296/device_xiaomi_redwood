874752c15fe30393bd93e0cd9bcf134e564c5f16
lolipuru <banstellazz@gmail.com>
2023-03-05 15:02:17 -0500

    redwood: Fix processor spacing issue & device name
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- init/init_redwood.cpp
+++ init/init_redwood.cpp
@@ -91,4 +91,7 @@ void vendor_load_properties() {
 
     // Set hardware revision
     property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
+    
+    // Superior-Extended OS
+    property_override("ro.superior.cpu", "Snapdragon™ 778G");
 }
