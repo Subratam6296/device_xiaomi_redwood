0f48b63a2380b4667ddf89ff34681827c721d902
Subratam6296 <padraramesh420@gmail.com>
2023-07-20 23:27:42 +0530

    redwood: parts: Import XiaomiParts
    Co-authored-by: PIPIPIG233666 <weikaik@umich.edu>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Signed-off-by: Subratam6296 <padraramesh420@gmail.com>

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+    
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # DRM
 PRODUCT_PACKAGES += \
