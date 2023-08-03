fa8f1ecf039646e29454224fd897db841a174a4c
kubersharma001 <kubersharma001@gmail.com>
2023-08-03 22:00:37 +0530

    redwood: Import powerhint.json from coral * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json

--- device.mk
+++ device.mk
@@ -456,8 +456,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
+    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json 
+    
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
