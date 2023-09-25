ae7c68c71fe447dbb8902e89dbf93f68b3a18bf7
rdx420 <padraramesh420@gmail.com>
2023-09-25 19:57:47 +0530

    redwood: Build missing thermal package
     * F linker  : CANNOT LINK EXECUTABLE /system_ext/bin/qspmsvc: library android.hardware.thermal@2.0.so not found: needed by main executable

--- device.mk
+++ device.mk
@@ -617,6 +617,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
+    
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0
 
 # WiFi Display
 PRODUCT_PACKAGES += \
