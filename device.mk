080efa780b68e1cd1a3ee8802bf5f9849ca1fc13
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-15 16:11:50 +0800

    redwood: Build vendor.qti.hardware.bluetooth.audio-V1-ndk
    *also update android.hardware.bluetooth@1.1.vendor

--- device.mk
+++ device.mk
@@ -110,8 +110,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
